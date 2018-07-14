66f09aa707cc9b379c52ca907d9f96fc7229a4b1
Jaypal Solanki <jaypsl263@gmail.com>
2018-07-14 01:49:54 -0400

    added template for name and add

--- client/main.js
+++ client/main.js
@@ -2,4 +2,28 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
+// import { Notes } from '../.meteor/lib/collection';
+import { Notes} from '../lib/collection.js';
 
+Template.body.helpers({
+  // notes : [
+  //   {text : 'my note 1'},
+  //   {text : 'my note 2'},
+  //   {text : 'my note 3'},
+  // ]
+  notes(){
+    return Notes.find({});
+  }
+})
+
+Template.add.events({
+  'submit .add-form' : function(){
+    event.preventDefault();
+    const target = event.target;
+    const text = target.text.value;
+    console.log(text);
+    
+    console.log(123);
+    return false;
+  }
+})
